fd88e93e0505bf20c92dac2be8f9e32f0aea4922
Mark <kotevode@icloud.com>
2017-02-08 14:42:38 +0500

    Package.swift changed

--- Package.swift
+++ Package.swift
@@ -3,7 +3,9 @@ import PackageDescription
 let package = Package(
     name: "MicrodataParser",
     dependencies: [
-		.Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", versions: Version(1,0,0)..<Version(3, .max, .max)),
 		.Package(url: "https://github.com/tid-kijyun/Kanna.git", majorVersion: 2)	
-    ]
+    ],
+	exclude: [
+		"Tests"
+	]
 )
